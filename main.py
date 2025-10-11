44c6eda233fb0abe01bdef3ad5deae04153b271a
cenmen <1074850787@qq.com>
2025-10-11 16:17:58 +0800

    chore: 更新.gitignore并注释掉CORS中间件配置
    在.gitignore中添加kaimen-admin.conf配置文件
    临时注释掉main.py中的CORS中间件配置以便测试

--- main.py
+++ main.py
@@ -22,18 +22,18 @@ app = FastAPI(
 )
 
 # 配置 CORS
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=[
-        "https://admin.kaimen.site",
-        "http://admin.kaimen.site", 
-        "https://admin-test.kaimen.site",
-        "http://admin-test.kaimen.site",
-    ],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=[
+#         "https://admin.kaimen.site",
+#         "http://admin.kaimen.site", 
+#         "https://admin-test.kaimen.site",
+#         "http://admin-test.kaimen.site",
+#     ],
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
 
 # 注册全局异常处理器
 app.add_exception_handler(RequestValidationError, validation_exception_handler)
